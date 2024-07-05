d2fcaee8a072510a52aee1ba59ea6493adf56f30
Salman <alfariqsalman@gmail.com>
2024-07-05 15:49:23 +0700

    edit footer active route icon

--- src/components/donor/Footer.vue
+++ src/components/donor/Footer.vue
@@ -1,54 +1,70 @@
 <template>
-    <ion-footer>
-        <ion-toolbar>
-            <div class="menu-button-list flex justify-between items-center w-full">
-                <div >
-                    <MenuButton :icon="HomeIcon" :label="'Beranda'" :route="'DonorHome'"></MenuButton>
-                </div>
-                <div >
-                    <MenuButton :icon="HistoryIcon" :label="'Riwayat'" :route="'History'"></MenuButton>
-                </div>
-                <div 
-                    v-if="session.isLoggedIn && user.data && user.data.roles && user.data.user_type === 'System User'">
-                    <button @click="openPopover" id="administrasi"
-                        class="flex flex-col items-center w-full p-2 hover:bg-gray-100 transition-colors duration-200">
-                        <div class="w-full flex justify-center items-center mb-1">
-                            <svg v-html="EventIcon" class="w-6 h-6"></svg>
-                        </div>
-                        <span class="text-xs">Administrasi</span>
-                    </button>
-                    <ion-popover trigger="administrasi" side="top" alignment="center" animated="false"
-                        :is-open="popoverOpen" @didDismiss="popoverOpen = false">
-                        <ion-content class="ion-padding">
-                            <h4 class="my-4 cursor-pointer" @click="toAccounting"
-                                v-if="user.data.roles.includes('Non Profit Accounting')">Akuntansi</h4>
-                            <h4 class="my-4 cursor-pointer" @click="toInventaris">Inventaris</h4>
-                            <h4 class="my-4 cursor-pointer" @click="toEventInput"
-                                v-if="user.data.roles.includes('Non Profit Secretary')">Kegiatan</h4>
-                        </ion-content>
-                    </ion-popover>
-                </div>
-                <div >
-                    <MenuButton :icon="AccountIcon" :label="'Akun'" :route="'Account'"></MenuButton>
-                </div>
-            </div>
-        </ion-toolbar>
-    </ion-footer>
+    <ion-tab-bar slot="bottom" class="py-1 bg-white shadow-2xl shadow-gray-500">
+        <ion-tab-button @click="router.push({ name: 'DonorHome' })" :class="[
+            'text-xs bg-white space-y-1.5 transition active:scale-95',
+            currentRoute.name === 'DonorHome'
+                ? 'border-green-600 text-green-600 font-bold'
+                : 'font-medium text-gray-700',
+        ]">
+            <component :is="HomeIcon" :class="currentRoute.name === 'DonorHome' ? 'text-green-600' : 'text-gray-700'"
+                class="h-5 w-5" />
+            <div>Beranda</div>
+        </ion-tab-button>
+        <ion-tab-button @click="router.push({ name: 'History' })" :class="[
+            'text-xs bg-white space-y-1.5 transition active:scale-95',
+            currentRoute.name === 'History'
+                ? 'border-green-600 text-green-600 font-bold'
+                : 'font-medium text-gray-700',
+        ]">
+            <component :is="HistoryIcon" :class="currentRoute.name === 'History' ? 'text-green-600' : 'text-gray-700'"
+                class="h-5 w-5" />
+            <div>Riwayat</div>
+        </ion-tab-button>
+        <ion-tab-button
+            v-if="session.isLoggedIn && user.data && user.data.roles && user.data.user_type === 'System User'"
+            @click="openPopover" id="administrasi" :class="[
+            'text-xs bg-white space-y-1.5 transition active:scale-95',
+            'font-medium text-gray-700'
+        ]">
+            <component :is="EventIcon" class="h-5 w-5" />
+            <div>Administrasi</div>
+        </ion-tab-button>
+        <ion-popover trigger="administrasi" side="top" alignment="center" animated="false" :is-open="popoverOpen"
+            @didDismiss="popoverOpen = false">
+            <ion-content class="ion-padding">
+                <h4 class="my-4 cursor-pointer" @click="toAccounting"
+                    v-if="user.data.roles.includes('Non Profit Accounting')">Akuntansi</h4>
+                <h4 class="my-4 cursor-pointer" @click="toInventaris">Inventaris</h4>
+                <h4 class="my-4 cursor-pointer" @click="toEventInput"
+                    v-if="user.data.roles.includes('Non Profit Secretary')">Kegiatan</h4>
+            </ion-content>
+        </ion-popover>
+        <ion-tab-button @click="router.push({ name: 'Account' })" :class="[
+            'text-xs bg-white space-y-1.5 transition active:scale-95',
+            currentRoute.name === 'Account'
+                ? 'border-green-600 text-green-600 font-bold'
+                : 'font-medium text-gray-700',
+        ]">
+            <component :is="AccountIcon" :class="currentRoute.name === 'Account' ? 'text-green-600' : 'text-gray-700'"
+                class="h-5 w-5" />
+            <div>Akun</div>
+        </ion-tab-button>
+    </ion-tab-bar>
 </template>
 
 <script setup>
 import { ref, inject } from 'vue';
-import { useRouter } from 'vue-router';
-import { IonFooter, IonToolbar, IonContent, IonPopover } from '@ionic/vue';
-import MenuButton from '@/components/MenuButton.vue';
-import HomeIcon from '@/components/icons/HomeIcon.svg?raw';
-import HistoryIcon from '@/components/icons/HistoryIcon.svg?raw';
-import AccountIcon from '@/components/icons/AccountIcon.svg?raw';
-import EventIcon from '@/components/icons/EventIcon.svg?raw';
+import { useRouter, useRoute } from 'vue-router';
+import { IonTabBar, IonTabButton, IonContent, IonPopover } from '@ionic/vue';
+import HomeIcon from '@/components/icons/HomeIcon.vue';
+import HistoryIcon from '@/components/icons/HistoryIcon.vue';
+import AccountIcon from '@/components/icons/AccountIcon.vue';
+import EventIcon from '@/components/icons/EventIcon.vue';
 
 const user = inject('$user');
 const session = inject('$session');
 const router = useRouter();
+const currentRoute = useRoute();
 
 const popoverOpen = ref(false);
 
@@ -71,19 +87,3 @@ const toAccounting = () => {
     router.push({ name: 'accountant' });
 };
 </script>
-
-<style scoped>
-.menu-button-list {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-}
-
-.custom-button {
-    margin: 3px;
-}
-
-ion-icon {
-    margin-bottom: 10px;
-}
-</style>
