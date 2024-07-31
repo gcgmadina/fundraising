cc5e07e4239b7f2d8cfabc3b682efe873db32f6a
Salman <alfariqsalman@gmail.com>
2024-07-31 17:42:50 +0700

    change CardListItem component for flexible use

--- src/components/CardListItem.vue
+++ src/components/CardListItem.vue
@@ -1,47 +1,56 @@
 <template>
   <ion-card class="flex flex-row-reverse flex-auto justify-around items-center">
+    <!-- Slot untuk status -->
     <div v-if="props.status" class="w-1/5">
-      {{ props.status }}
+      <slot name="status">
+        {{ props.status }}
+      </slot>
     </div>
 
-    <div class="title-section items-center w-3/5">
-      <ion-card-title>{{ props.title }}</ion-card-title>
-      <div class="flex flex-auto justify-between items-center">
-        <ion-card-subtitle>{{ props.subtitle }}</ion-card-subtitle>
+    <!-- Slot untuk title -->
+    <div class="title-section flex flex-col w-3/5 p-4">
+      <slot name="title">
+        <ion-card-title class="text-xl font-bold">{{ props.title }}</ion-card-title>
+      </slot>
+      <div class="flex justify-between items-center w-full mt-2">
+        <slot name="subtitle">
+          <ion-card-subtitle class="text-sm text-gray-500">{{ props.subtitle }}</ion-card-subtitle>
+        </slot>
       </div>
     </div>
 
-    <div v-if="props.image" class="image-section flex items-center w-2/5">
-      <ion-img :src="props.image" class="card-image"></ion-img>
+    <!-- Slot untuk image -->
+    <div v-if="props.image" class="image-section flex items-center p-4">
+      <slot name="image">
+        <!-- Cek tipe data props.image dan gunakan komponen atau gambar -->
+        <component :is="imageComponent" :src="props.image" class="w-full h-auto"></component>
+      </slot>
     </div>
   </ion-card>
 </template>
 
 <script setup>
-import { defineProps } from 'vue';
-import { IonCard, IonCardSubtitle, IonCardTitle, IonButton, IonImg } from '@ionic/vue';
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
+import { defineProps, computed } from 'vue';
+import { IonCard, IonCardSubtitle, IonCardTitle, IonImg } from '@ionic/vue';
 
 const props = defineProps({
-  title: { required: true },
-  subtitle: { required: true },
+  title: { required: false },
+  subtitle: { required: false },
   image: { required: false },
   nextPage: { required: false },
   id: { required: false },
   status: { required: false }
 });
+
+const imageComponent = computed(() => {
+  // Jika props.image adalah string, gunakan IonImg
+  // Jika props.image adalah objek atau fungsi (misalnya komponen icon), gunakan props.image secara langsung
+  return typeof props.image === 'string' ? IonImg : props.image;
+});
 </script>
 
 <style scoped>
-.title-section {
-  padding: 15px 15px;
-  margin: 0;
-}
-
 .image-section {
-  padding: 0 15px;
-  margin: 0;
+  max-width: 40%;
 }
 </style>
