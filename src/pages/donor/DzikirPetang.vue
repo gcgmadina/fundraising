804477df0a3344afd88291595a39f59d05343a24
Salman <alfariqsalman@gmail.com>
2024-08-02 21:48:07 +0700

    add qur'an and dzikir latin teks

--- src/pages/donor/DzikirPetang.vue
+++ src/pages/donor/DzikirPetang.vue
@@ -11,26 +11,31 @@
                 أَعُوذُ بِاللَّهِ مِنَ الشَّيْطَانِ الرَّجِيمِ
 
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">Audzubillahiminasyaitonirojim</p>
             <p class="ml-5">Artinya: "Aku berlindung kepada Allah dari godaan setan yang terkutuk."</p>
 
             <h2 class="font-bold">2. Ayat Kursi</h2>
             <p class="text-2xl ml-5 my-4 text-right">
                 اَللّٰهُ لَآ اِلٰهَ اِلَّا هُوَۚ اَلْحَيُّ الْقَيُّوْمُ ەۚ لَا تَأْخُذُهٗ سِنَةٌ وَّلَا نَوْمٌۗ لَهٗ مَا فِى السَّمٰوٰتِ وَمَا فِى الْاَرْضِۗ مَنْ ذَا الَّذِيْ يَشْفَعُ عِنْدَهٗٓ اِلَّا بِاِذْنِهٖۗ يَعْلَمُ مَا بَيْنَ اَيْدِيْهِمْ وَمَا خَلْفَهُمْۚ وَلَا يُحِيْطُوْنَ بِشَيْءٍ مِّنْ عِلْمِهٖٓ اِلَّا بِمَا شَاۤءَۚ وَسِعَ كُرْسِيُّهُ السَّمٰوٰتِ وَالْاَرْضَۚ وَلَا يَـُٔوْدُهٗ حِفْظُهُمَاۚ وَهُوَ الْعَلِيُّ الْعَظِيْمُ ٢٥٥
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">Allahu laa ilaa haillaa huwa hayyul qoyyuum, laa ta'khudzu sinatuu wa laa naum, lahuu maa fissamaawaati wamaa fil ardhi, mandzalladzii yasyfa'u 'indahu illaa bi'idznihi ya'lamu maa baina aidiihim wa maa khalfahum, wa laa yukhiithuuna bi syai'im min 'ilmihi illa bi maa syaa', wa si'a kursiyyuhus samaawaati wal ardhi, wa laa yaudhuhu hifdzu humaa wa huwal aliyyul 'adhiim.</p>
             <p class="ml-5">Artinya: "Allah, tidak ada tuhan selain Dia, Yang Mahahidup lagi terus-menerus mengurus (makhluk-Nya). Dia tidak dilanda oleh kantuk dan tidak (pula) oleh tidur. Milik-Nyalah apa yang ada di langit dan apa yang ada di bumi. Tidak ada yang dapat memberi syafaat di sisi-Nya tanpa izin-Nya. Dia mengetahui apa yang ada di hadapan mereka dan apa yang ada di belakang mereka. Mereka tidak mengetahui sesuatu apa pun dari ilmu-Nya, kecuali apa yang Dia kehendaki. Kursi-Nya (ilmu dan kekuasaan-Nya) meliputi langit dan bumi. Dia tidak merasa berat memelihara keduanya. Dialah yang Mahatinggi lagi Mahaagung."</p>
 
             <h2 class="font-bold">3. Surah Al Ikhlas, Surah Al Falaq, dan Surah An Nas (3x)</h2>
             <p class="text-2xl ml-5 my-4 text-right">
                 قُلْ هُوَ اللّٰهُ اَحَدٌۚ ١ اَللّٰهُ الصَّمَدُۚ ٢ لَمْ يَلِدْ وَلَمْ يُوْلَدْۙ ٣ وَلَمْ يَكُنْ لَّهٗ كُفُوًا اَحَدٌ ࣖ ٤
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">Qul huwa allahu ahad, allahus shamad, lam yalid wa lam yuulad, wa lam ya kullahu kufuwan ahad.</p>
             <p class="ml-5">Artinya: "Katakanlah (Nabi Muhammad), "Dialah Allah Yang Maha Esa. Allah tempat meminta segala sesuatu. Dia tidak beranak dan tidak pula diperanakkan serta tidak ada sesuatu pun yang setara dengan-Nya." (QS Al Ikhlas: 1-4)</p>
             <p class="text-2xl ml-5 my-4 text-right">
                 قُلْ اَعُوْذُ بِرَبِّ الْفَلَقِۙ ١ مِنْ شَرِّ مَا خَلَقَۙ ٢ وَمِنْ شَرِّ غَاسِقٍ اِذَا وَقَبَۙ ٣ وَمِنْ شَرِّ النَّفّٰثٰتِ فِى الْعُقَدِۙ ٤ وَمِنْ شَرِّ حَاسِدٍ اِذَا حَسَدَ ࣖ ٥
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">Qul a'uudzu birabbil falaq, min syarri maa khalaq, wa min syarri ghaasiqin idza waqab, wa min syarrin naffathaati fil 'uqad, wa min syarri haasidin idza hasad.</p>
             <p class="ml-5">Artinya: "Katakanlah (Nabi Muhammad), "Aku berlindung kepada Tuhan yang (menjaga) fajar (subuh) dari kejahatan (makhluk yang) Dia ciptakan, dari kejahatan malam apabila telah gelap gulita, dari kejahatan perempuan-perempuan (penyihir) yang meniup pada buhul-buhul (talinya),dan dari kejahatan orang yang dengki apabila dia dengki." (QS Al Falaq: 1-5)</p>
             <p class="text-2xl ml-5 my-4 text-right">
                 قُلْ اَعُوْذُ بِرَبِّ النَّاسِۙ ١ مَلِكِ النَّاسِۙ ٢ اِلٰهِ النَّاسِۙ ٣ مِنْ شَرِّ الْوَسْوَاسِ الْخَنَّاسِۙ ٤ الَّذِىْ يُوَسْوِسُ فِى صُدُوْرِ النَّّاسِ ٥ مِنَ الْجِنَّةِ وَالنَّاسِ ٦
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">Qul a'uudzu bi rabbin naas, malikin naas, ilahinaas, min syarril waswaasil khannaas, alladzii yuwaswisu fii shuduurin naas, minal jinnati wannaas.</p>
             <p class="ml-5">Artinya: "Katakanlah (Nabi Muhammad), "Aku berlindung kepada Tuhan (yang memelihara dan menguasai) manusia. Raja manusia. Sembahan (Ilah) manusia. Dari kejahatan (bisikan) syaitan yang biasa bersembunyi, yang membisikkan (kejahatan) ke dalam dada-dada manusia, dari jin dan manusia." (QS An Nas: 1-6)</p>
 
             <h2 class="font-bold">4. Dzikir Petang Pertama</h2>
@@ -39,6 +44,7 @@
                 أَمْسَيْنَا عَلَى فِطْرَةِ اْلإِسْلاَمِ وَعَلَى كَلِمَةِ اْلإِخْلاَصِ، وَعَلَى دِيْنِ نَبِيِّنَا مُحَمَّدٍ صَلَّى اللهُ عَلَيْهِ وَسَلَّمَ، وَعَلَى مِلَّةِ أَبِيْنَا إِبْرَاهِيْمَ، حَنِيْفًا مُسْلِمًا وَمَا كَانَ مِنَ الْمُشْرِكِيْنَ
 
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">Amsaynaa 'ala fithrotil Islam, wa 'ala kalimatil ikhlash, wa 'ala diini nabiiyinaa muhammad shallallahu alaihi wa sallam, wa 'ala millati abaina ibrahim haniifan muslimaw wa maa kaana minal musyrikiin.</p>
             <p class="ml-5">Artinya: "Di waktu petang kami berada diatas fitrah agama Islam, kalimat ikhlas, agama Nabi kami Muhammad dan agama ayah kami, Ibrahim, yang berdiri di atas jalan yang lurus, muslim dan tidak tergolong orang-orang musyrik."</p>
 
             <h2 class="font-bold">5. Dzikir Petang Kedua</h2>
@@ -47,6 +53,7 @@
                 رَضِيْتُ بِاللهِ رَبًّا، وَبِالإِسْلَامِ دِيْنًا، وَبِمُحَمَّدٍ نَبِيًّا
 
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">Radhiitu billahi rabba, wa bil islaama diina, wa bimuhammadin nabiyya.</p>
             <p class="ml-5">
                 Artinya: "Aku rela (rida) Allah sebagai Rabb-ku (untukku dan orang lain), Islam sebagai agamaku dan Muhammad sebagai Nabiku (yang diutus oleh Allah)."
             </p>
@@ -57,6 +64,7 @@
                 اللَّهُمَّ بِكَ أَمْسَيْنَا وَبِكَ أَصْبَحْنَا ، وَبِكَ نَحْيَا وَبِكَ نَمُوتُ، وَإِلَيْكَ الْمَصِيْرُ
 
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">Allahumma bika amsaynaa wa bika ashbahnaa wa bika nahyaa, wa bika namuutu wa ilaikal mashir.</p>
             <p class="ml-5">
                 Artinya: "Ya Allah, dengan rahmat dan pertolongan-Mu kami memasuki waktu petang, dan dengan rahmat dan pertolongan-Mu kami memasuki waktu pagi. Dengan rahmat dan kehendak-Mu kami hidup dan dengan rahmat dan kehendak-Mu kami mati. Dan kepada-Mu tempat kembali (bagi semua makhluk)."
             </p>
@@ -67,6 +75,7 @@
                 أَمْسَيْنَا وَأَمْسَى الْمُلْكُ لِلَّهِ، وَالْحَمْدُ لِلَّهِ، لاَ إِلَـهَ إِلاَّ اللهُ وَحْدَهُ لاَ شَرِيْكَ لَهُ، لَهُ الْمُلْكُ وَلَهُ الْحَمْدُ وَهُوَ عَلَى كُلِّ شَيْءٍ قَدِيْرُ. رَبِّ أَسْأَلُكَ خَيْرَ مَا فِيْ هَذِهِ اللَّيْلَةِ وَخَيْرَ مَا بَعْدَهَا، وَأَعُوْذُ بِكَ مِنْ شَرِّ مَا فِي هَذِهِ اللَّيْلَةِ وَشَرِّ مَا بَعْدَهَا، رَبِّ أَعُوْذُ بِكَ مِنَ الْكَسَلِ وَسُوْءِ الْكِبَرِ، رَبِّ أَعُوْذُ بِكَ مِنْ عَذَابٍ فِي النَّارِ وَعَذَابٍ فِي الْقَبْرِ
 
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">Amsaynaa wa amsal mulku lillahi, walhamdulillah, laa ilahaillallah wahdahu laa syariikalahu, lahul mulku wa lahulhamdu, wa huwa 'ala kulli syai'in qadiir, rabbi as'aluka khaira maa fii hadzal yaumi wa khaira maa ba'dahu, wa a'uudzu bika min syarri maa fii hadzal yaumi wa syarrimaa ba'dahu, rabbi a'uudzubika minal kasali, wa suu'ul kibari, rabbi a'uudzu bika min 'adzaabin naari, wa 'adzaabilqabri.</p>
             <p class="ml-5">
                 Artinya: "Kami telah memasuki waktu petang dan kerajaan hanya milik Allah, segala puji bagi Allah. Tidak ada Tuhan (yang berhak disembah) kecuali Allah Yang Maha Esa, tiada sekutu bagi-Nya. Bagi-Nya kerajaan dan bagi-Nya pujian. Dia-lah Yang Mahakuasa atas segala se-suatu. Hai Tuhan, aku mohon kepada-Mu kebaikan di hari ini dan kebaikan sesudahnya. Aku berlindung kepadaMu dari kejahatan hari ini dan kejahatan sesudahnya. Wahai Tuhan, aku berlindung kepadaMu dari kemalasan dan kejelekan di hari tua. Wahai Tuhan! Aku berlindung kepadaMu dari siksaan di Neraka dan kubur."
             </p>
@@ -77,6 +86,7 @@
                 اللَّهُمَّ أَنْتَ رَبِّي لَا إِلَهَ إِلَّا أَنْتَ خَلَقْتَنِي وَأَنَا عَبْدُكَ وَأَنَا عَلَى عَهْدِكَ وَوَعْدِكَ مَا اسْتَطَعْتُ أَعُوذُ بِكَ مِنْ شَرِّ مَا صَنَعْتُ أَبُوءُ لَكَ بِنِعْمَتِكَ عَلَيَّ وَأَبُوءُ لَكَ بِذَنْبِي فَاغْفِرْ لِي فَإِنَّهُ لَا يَغْفِرُ الذُّنُوبَ إِلَّا أَنْتَ
 
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">Allahumma anta rabbi laa ilaa hailla anta, khalaqtanii wa ana 'abduka, wa ana 'ala 'ahdika wa wa'dika mas tatho'tu. 'a uudzu bika min syarrimaa shana'tu, abuu 'u laka bini'matika 'alayya wa abuu'u bidhanbii, faghfirlii, fainnahu laa yaghfiru dzunuuba illa anta.</p>
             <p class="ml-5">Artinya: "Ya Allah, Engkau lah Tuhanku. Tidak ada Tuhan selain Engkau. Engkau yang menciptakan aku dan aku adalah hamba-Mu. Aku berada di dalam hidayahmu, dan perjanjian dengan-Mu. Sebisa yang aku mampu. Aku berlindung kepada-Mu, dari segala kejelekan yang aku perbuat. Aku bersyukur atas nikmat yang Engkau limpahkan kepada kami, dan aku menyesal atas segala yang dosa yang aku perbuat. Maka ampunilah aku, sesungguhnya tidak ada yang dapat mengampuni dosa selain Engkau."</p>
 
             <h2 class="font-bold">9. Dzikir Petang Keenam</h2>
@@ -85,6 +95,7 @@
                 اللَّهُمَّ إنِّي أَسْأَلُكَ العَفْوَ وَالعَافِيَةَ فِي الدُّنْيَا وَالآخِرَةِ، اللَّهُمَّ إنِّي أَسْأَلُكَ العَفْوَ وَالعَافِيَةَ فِي دِينِي وَدُنْيَايَ وَأَهْلِي وَمَالِي، اللَّهُمَّ اسْتُرْ عَوْرَاتِي، وَآمِنْ رَوْعَاتِي، اللَّهُمَّ احْفَظْنِي مِنْ بَيْنَ يَدَيَّ، وَمِنْ خَلْفِي، وَعَنْ يَمِينِي، وَعَنْ شِمَالِي، وَمِنْ فَوْقِي، وَأَعُوذُ بِعَظَمَتِكَ أَنْ أُغْتَالَ مِنْ تَحْتِي
 
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">Allahumma inni as-alukal 'afwa wal 'afiyata fid dunya wal akhiroh. Allahumma inni as-alukal a'fwa wal 'afiyata fi dini wa dunyaya wa ahli wa mali. Allahummastur 'auroti wa amin row'ati. Allahummahfadzni min baini yadayya wa min kholfi wa 'an yamini wa 'an syimali wa min fauqi wa a'uzubika bi 'adzomatika an ughtala min tahti.</p>
             <p class="ml-5">Artinya: "Ya Allah, sesungguhnya aku memohon kebajikan dan keselamatan di dunia dan akhirat. Ya Allah, sesungguhnya aku memohon kebaikan dan keselamatan dalam agama, dunia, keluarga dan hartaku. Ya Allah, tutupilah auratku (aib dan sesuatu yang tidak layak dilihat orang) dan tentramkan-lah aku dari rasa takut. Ya Allah, peliharalah aku dari depan, belakang, kanan, kiri dan dari atasku. Aku berlindung dengan kebesaran-Mu, agar aku tidak disambar dari bawahku (aku berlindung dari dibenamkan ke dalam bumi)."</p>
 
             <h2 class="font-bold">10. Dzikir Petang Ketujuh (3x)</h2>
@@ -93,6 +104,7 @@
                 اللَّهُمَّ عَافِنِي فِي بَدَنِي، اللَّهُمَّ عَافِنِي فِي سَمْعِي، اللَّهُمَّ عَافِنِي فِي بَصَرِي، لَا إِلَهَ إِلَّا أَنْتَ اَللّٰھُمَّ اِنِّیْ اَعُوْذُبِکَ مِنَ الْکُفْرِ وَالْفَقْرِ ، اَللّٰھُمَّ اِنِّی اَعُوْذُ بِکَ مِنَ عَذَابِ الْقَبْرِ، لَا اِلَہَ اِلَّا اَنْتَ
 
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">Allahumma 'aafinii fii badani, Allahumma 'aafini fii sam'ii, Allahumma 'aafini fii basharii, laa ilaa haillah anta, Allahumma inni 'auudzubika minal kufri walfaqri, Allahumma innii 'audzubika min'adzaabil qabri, laa ilahaillaa anta.</p>
             <p class="ml-5">Artinya: "Ya Allah, selamatkanlah tubuhku (dari penyakit dan dari apa yang tidak aku inginkan). Ya Allah, selamatkanlah pendengaranku. Ya Allah, selamatkanlah penglihatanku, tidak ada Ilah yang berhak diibadahi dengan benar kecuali Engkau. Ya Allah, sesungguhnya aku berlindung kepada-Mu dari kekufuran dan kefakiran. Aku berlindung kepada-Mu dari siksa kubur, tidak ada Ilah yang berhak diibadahi dengan benar kecuali Engkau.</p>
 
             <h2 class="font-bold">11. Dzikir Petang Kedelapan</h2>
@@ -101,6 +113,7 @@
                 اَللَّهُمَّ عَالِمَ الْغَيْبِ وَالشَّهَادَةِ فَاطِرَ السَّمَاوَاتِ وَاْلأَرْضِ، رَبَّ كُلِّ شَيْءٍ وَمَلِيْكَهُ، أَشْهَدُ أَنْ لاَ إِلَـهَ إِلاَّ أَنْتَ، أَعُوْذُ بِكَ مِنْ شَرِّ نَفْسِيْ، وَمِنْ شَرِّ الشَّيْطَانِ وَشِرْكِهِ، وَأَنْ أَقْتَرِفَ عَلَى نَفْسِيْ سُوْءًا أَوْ أَجُرَّهُ إِلَى مُسْلِمٍ
 
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">Allahumma 'aalimal ghaibi was syahaadah, faathiris samaawaati wal ardhi, rabbi kulli syain wa maliikahu, asyhadu allaa ilaa hailla anta, a'udzuu bika min syarri nafsii, wa syarri sysyaitahaai wa syirkihi, wa an aqtarifa 'alaa nafsi suu'aan au ajurrohu ila muslim.</p>
             <p class="ml-5">Artinya: "Ya Allah Yang Mahamengetahui yang ghaib dan yang nyata, wahai Rabb Pencipta langit dan bumi, Rabb atas segala sesuatu dan Yang Merajainya. Aku bersaksi bahwa tidak ada Ilah yang berhak diibadahi dengan benar kecuali Engkau. Aku berlindung kepada-Mu dari kejahatan diriku, setan dan ajakannya menyekutukan Allah (aku berlindung kepada-Mu) dari berbuat kejelekan atas diriku atau mendorong seorang muslim kepadanya.</p>
 
             <h2 class="font-bold">12. Dzikir Petang Kesembilan (4x)</h2>
@@ -109,6 +122,7 @@
                 بِسْمِ اللَّهِ الَّذِى لاَ يَضُرُّ مَعَ اسْمِهِ شَىْءٌ فِى الأَرْضِ وَلاَ فِى السَّمَاءِ وَهُوَ السَّمِيعُ الْعَلِيمُ
 
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">Bismillahi laa yadhurru ma'a smihi syai'un fil ardhi wa laa fis samaa'i wa huwa ssmii'ul aliim.</p>
             <p class="ml-5">Artinya: "Dengan Menyebut Nama Allah, yang dengan Nama-Nya tidak ada satupun yang membahayakan, baik di bumi maupun di langit. Dia-lah Yang Maha mendengar dan Maha mengetahui."</p>
 
             <h2 class="font-bold">13. Dzikir Petang Kesepuluh</h2>
@@ -117,6 +131,7 @@
                 يَا حَيُّ يَا قَيُّوْمُ بِرَحْمَتِكَ أَسْتَغِيْثُ، وَأَصْلِحْ لِيْ شَأْنِيْ كُلَّهُ وَلاَ تَكِلْنِيْ إِلَى نَفْسِيْ طَرْفَةَ عَيْنٍ
 
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">Ya hayyun ya qayyum, birahmatika astaghiistu, ashlih lii sya'nii kullihi walaa takilnii ilaa nafsii tharfata 'ainin.</p>
             <p class="ml-5">Artinya: "Wahai Rabb Yang Maha Hidup, wahai Rabb Yang Berdiri Sendiri (tidak butuh segala sesuatu), dengan rahmat-Mu aku minta pertolongan, perbaikilah segala urusanku dan jangan diserahkan kepadaku sekalipun sekejap mata (tanpa mendapat pertolongan dari-Mu).</p>
 
             <h2 class="font-bold">14. Dzikir Kesebelas (3x)</h2>
@@ -125,6 +140,7 @@
                 أَعُوْذُ بِكَلِمَاتِ اللهِ التَّآمَّاتِ مِنْ شَرِّ مَا خَلَقَ
 
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">A'udzu bikalimaatil lahit tammaa ti mins syarri maa kholaq.</p>
             <p class="ml-5">Artinya: "Aku berlindung dengan kalimat-kalimat Allah yang sempurna, dari kejahatan sesuatu yang diciptakan-Nya."</p>
 
             <h2 class="font-bold">15. Dzikir Petang Kedua Belas (100x)</h2>
@@ -133,6 +149,7 @@
                 سُبْحَانَ اللهِ وَبِحَمْدِهِ
 
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">Subhanallahi wabihamdihi.</p>
             <p class="ml-5">Artinya: "Mahasuci Allah, aku memuji-Nya."</p>
 
             <h2 class="font-bold">16. Dzikir Petang Ketiga Belas (3x)</h2>
@@ -141,6 +158,7 @@
                 لاَ إِلَـهَ إِلاَّ اللهُ وَحْدَهُ لاَ شَرِيْكَ لَهُ، لَهُ الْمُلْكُ وَلَهُ الْحَمْدُ وَهُوَ عَلَى كُلِّ شَيْءٍ قَدِيْرُ
 
             </p>
+            <p class="ml-5 my-3 text-gray-800 italic">Laa ilaahaillallah wahdahu laa syariikalahu, lahul mulku walahul hamdu wa huwa alaa kulli syain qadiir.</p>
             <p class="ml-5">Artinya: "Tidak ada Tuhan selain Allah, Yang Mahaesa tidak ada sekutu bagi-Nya, milik-Nyalah segala kerajaan dan bagi-Nya pujian dan Dia Mahaberkuasa atas segala sesuatu."</p>
 
             <div class="mt-10 mb-4"> Referensi: <a href="https://www.detik.com/hikmah/doa-dan-hadits/d-7464040/doa-dzikir-pagi-dan-petang-sesuai-sunnah-untuk-pembuka-rezeki" target="_blank">detik.com</a></div>
