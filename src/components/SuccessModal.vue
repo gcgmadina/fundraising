92951c9100999f45bc7bac0f03f7fabcd84827c6
Salman <alfariqsalman@gmail.com>
2024-08-08 15:53:14 +0700

    add address success modal

---
+++ src/components/SuccessModal.vue
@@ -0,0 +1,40 @@
+<!-- src/components/Modal.vue -->
+<template>
+    <div v-if="isModalOpen" class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+        <div class="flex flex-col justify-center bg-white p-6 rounded-lg shadow-lg w-full max-w-md w-4/5">
+            <component :is="validationSuccess ? SuccessIcon : FailedIcon" class="w-20 h-20 mx-auto mb-4" />
+            <p v-if="validationSuccess" class="text-center text-2xl">{{ successMessage }}</p>
+            <p v-else class="text-center text-2xl">{{ failureMessage }}</p>
+            <ion-button class="mt-4" @click="closeModal">Tutup</ion-button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { IonButton } from '@ionic/vue';
+import SuccessIcon from '@/components/icons/SuccessIcon.vue';
+import FailedIcon from '@/components/icons/FailedIcon.vue';
+
+const props = defineProps({
+    isModalOpen: {
+        type: Boolean,
+        required: true,
+    },
+    validationSuccess: {
+        type: Boolean,
+        required: true,
+    },
+    closeModal: {
+        type: Function,
+        required: true,
+    },
+    successMessage: {
+        type: String,
+        default: 'Operasi Berhasil',
+    },
+    failureMessage: {
+        type: String,
+        default: 'Operasi Gagal',
+    },
+});
+</script>
--- src/pages/secretary/AddressInput.vue
+++ src/pages/secretary/AddressInput.vue
@@ -47,6 +47,14 @@
             <div class="flex justify-end">
                 <ion-button @click="submitForm">Simpan</ion-button>
             </div>
+
+            <SuccsessModal 
+                :isModalOpen="isModalOpen" 
+                :validationSuccess="validationSuccess" 
+                :closeModal="closeModal"
+                :successMessage="successMessage"
+                :failureMessage="failureMessage"
+            />
         </ion-content>
         <Footer />
     </ion-page>
@@ -54,10 +62,12 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 import { IonPage, IonContent, IonList, IonItem, IonInput, IonButton, IonSelect, IonSelectOption } from '@ionic/vue';
 import Header from '@/components/Header.vue';
 import Footer from '@/components/donor/Footer.vue';
 import { fetchAllCities, searchCity, addAddress, editMosqueAddress, getMosqueAddress } from '@/data/masjid/Address';
+import SuccsessModal from '@/components/SuccessModal.vue';
 
 const address = ref({
     name: '',
@@ -69,6 +79,8 @@ const address = ref({
     phone: '',
     email: ''
 });
+
+const router = useRouter();
 
 const citySearch = ref('');
 const allCities = ref([]);
@@ -122,22 +134,47 @@ onMounted(() => {
         });
 });
 
+
+const isModalOpen = ref(false);
+const validationSuccess = ref(false);
+const closeModal = () => {
+    isModalOpen.value = false;
+
+    if (validationSuccess.value) {
+        router.back();
+    }
+};
+const successMessage = ref('');
+const failureMessage = ref('');
+
 const submitForm = () => {
     if (isEditMode.value) {
         editMosqueAddress(address.value)
             .then((message) => {
                 console.log('Address edited:', message);
+                successMessage.value = 'Alamat berhasil diubah';
+                validationSuccess.value = true;
+                isModalOpen.value = true;
             })
             .catch((error) => {
                 console.error('Error editing address:', error);
+                failureMessage.value = 'Gagal mengubah alamat';
+                validationSuccess.value = false;
+                isModalOpen.value = true;
             });
     } else {
         addAddress(address.value, true)
             .then((message) => {
                 console.log('Address added:', message);
+                successMessage.value = 'Alamat berhasil ditambahkan';
+                validationSuccess.value = true;
+                isModalOpen.value = true;
             })
             .catch((error) => {
                 console.error('Error adding address:', error);
+                failureMessage.value = 'Gagal menambahkan alamat';
+                validationSuccess.value = false;
+                isModalOpen.value = true;
             });
     }
 };
