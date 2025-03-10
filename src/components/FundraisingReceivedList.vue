5b5c25a380e2386a56c939b8d4ae4ffe04b68528
SalmanDA <alfariqsalman@gmail.com>
2025-03-10 13:28:09 +0700

    refactor FundraisingReceivedList component layout; enhance card structure and improve display of fundraising details

--- src/components/FundraisingReceivedList.vue
+++ src/components/FundraisingReceivedList.vue
@@ -1,6 +1,7 @@
 <template>
-    <ion-card v-for="(entry, index) in journalEntry.list.data" class="flex flex-row" @click="router.push({ name: 'FundraisingReceivedDetail', params: { id: entry.name } })">
-        <img :src="entry.Fundraising_thumbnail" alt="Gambar penggalangan dana" class="w-1/5 mx-auto">
+    <ion-card v-for="(entry, index) in journalEntry.list.data"
+        @click="router.push({ name: 'FundraisingReceivedDetail', params: { id: entry.name } })">
+        <!-- <img :src="entry.Fundraising_thumbnail" alt="Gambar penggalangan dana" class="w-1/5 mx-auto">
         <ion-card-content class="w-3/5 flex flex-col justify-between" mode="ios">
             <ion-card-title class="text-lg ">{{ entry.fundraising_name }}</ion-card-title>
                 <p>{{ entry.posting_date }}</p>
@@ -12,17 +13,36 @@
             'bg-green-500': entry.docstatus === 'Diterima'
         }">
             <p>{{ entry.docstatus }}</p>
-        </div>
+        </div> -->
 
+        <div class="flex flex-row justify-between items-center p-2 w-full">
+            <ion-card-header class="w-[30%]">
+                <ion-card-title class="text-violet-800 text-sm w-full overflow-hidden text-ellipsis line-clamp-2">
+                    {{ entry.fundraising_name }}
+                </ion-card-title>
+
+            </ion-card-header>
+
+            <ion-card-content class="p-0 w-[60%]">
+                <div class="flex flex-col justify-center items-center text-sm my-2">
+                    <p class="overflow-hidden whitespace-nowrap text-ellipsis">Rp.{{
+                        formatCurrency(entry.total_debit) }}</p>
+                    <p class="overflow-hidden whitespace-nowrap text-ellipsis">{{ entry.docstatus }}</p>
+                </div>
+            </ion-card-content>
+
+            <ion-icon class="w-[10%]" :icon="chevronForward" size="large" color="tertiary"></ion-icon>
+        </div>
     </ion-card>
 </template>
 
 <script setup>
-import { IonList, IonCard, IonCardTitle, IonCardSubtitle, IonCardContent, IonInfiniteScroll, IonInfiniteScrollContent } from "@ionic/vue"
+import { IonIcon, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonInfiniteScroll, IonInfiniteScrollContent } from "@ionic/vue"
 import { onMounted } from 'vue'
 import { journalEntry } from '@/data/donation/Fundraising'
 import { formatCurrency } from '@/data/utils';
 import { useRouter } from 'vue-router'
+import { chevronForward } from 'ionicons/icons';
 
 const router = useRouter()
 
