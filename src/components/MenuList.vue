ccfdef8a166082e360ff83b42a1d1692a34aa586
SalmanDA <alfariqsalman@gmail.com>
2025-03-12 11:57:20 +0700

    refactor MenuList component to conditionally display menus based on user roles and session status

--- src/components/MenuList.vue
+++ src/components/MenuList.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="border-2 p-4 rounded-lg bg-white">
-        <div class="flex flex-row flex-wrap justify-center gap-4 text-center">
+        <div class="flex flex-row flex-wrap justify-around gap-4 text-center">
             <div v-for="menu in menus" :key="menu.title" @click="navigate(menu.link)"
                 class="flex flex-col items-center cursor-pointer transition-transform hover:scale-110 w-20">
                 <div class="w-14 h-14 bg-gray-100 rounded-full flex items-center justify-center">
@@ -15,19 +15,40 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, inject, computed } from "vue";
 import { useRouter } from "vue-router";
 import Solat from "@/components/icons/solat.svg";
 import Tasbih from "@/components/icons/tasbih.svg";
 import Quran from "@/components/icons/quran.svg";
+import Berita from "@/components/icons/berita.svg"
+import Kajian from "@/components/icons/kajian.svg"
+import PenggalanganDana from "@/components/icons/penggalangan_dana.svg"
+import ProfilMasjid from "@/components/icons/profil_masjid.svg"
 
 const router = useRouter();
+const user = inject('$user');
+const session = inject('$session');
 
-const menus = ref([
+const guestMenus = ref([
     { title: "Jadwal Solat", icon: Solat, link: "PrayerTimes" },
     { title: "Dzikir", icon: Tasbih, link: "Dzikir" },
     { title: "Al-quran", icon: Quran, link: "SurahList" }
 ]);
+
+const secretaryMenus = ref([
+    { title: "Profil Masjid", icon: ProfilMasjid, link: "MasjidProfile" },
+    { title: "Berita", icon: Berita, link: "NewsList" },
+    { title: "Kajian", icon: Kajian, link: "KajianList" },
+    { title: "Penggalangan Dana", icon: PenggalanganDana, link: "FundraisingList" }
+])
+
+const menus = computed(() => {
+    if (session?.isLoggedIn && user?.data?.roles?.includes('Non Profit Secretary')) {
+        return [...secretaryMenus.value, ...guestMenus.value]; 
+    } else {
+        return guestMenus.value;
+    }
+});
 
 const navigate = (route) => {
     router.push({ name: route });
